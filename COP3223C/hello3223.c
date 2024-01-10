29fa1251e3670437c2a97cc216af5218428a5a7b
Yauheni Khvashcheuski <evgenijhq@gmail.com>
2024-01-10 12:44:21 -0500

    creates the structure

---
+++ COP3223C/hello3223.c
@@ -0,0 +1,15 @@
+/*
+  Yauheni Khvashcheuski
+  COP3223C
+  Section 3
+  Assignment #0 Hello World!
+  01/12/2024
+  The program prints "Hello 3223 class!"
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Prints to the console
+    printf("Hello 3223 class!");
+}
