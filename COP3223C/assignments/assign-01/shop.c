7ad8597371972afd7601c0932012b8aae31a0cc2
Yauheni Khvashcheuski <evgenijhq@gmail.com>
2024-01-15 18:15:17 -0500

    assignments/assign-01

---
+++ COP3223C/assignments/assign-01/shop.c
@@ -0,0 +1,7 @@
+/*
+    Yauheni Khvashcheuski
+    COP3223C Section 3
+    01/24/2024
+    Assignment 1, Part A
+    
+*/
---
+++ COP3223C/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  float a = 1.0;
+  printf("%8.4f", a);
+}
